e651e2fb4c1f2abc247b984333d6e7c02e5e57d8
snakista <sitanshyadav@gmail.com>
2020-12-23 16:58:38 +0530

    File added

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import {fetchData} from './api';
+import coronaimg from './Images/image.png';
+
+class App extends React.Component{
+    state={
+        data: {},
+        country: '',
+    }
+    async componentDidMount(){
+        const fetchedData=await fetchData();
+        this.setState({data: fetchedData});
+    }
+    handleCountryChange=async(country)=>{
+        const fetchedData=await fetchData(country);
+        this.setState({data: fetchedData, country: country});
+    }
+    render(){
+        const {data, country}=this.state;
+        return(
+            <div className={styles.container}>
+            <img src={coronaimg} className={styles.image} alt="COVID-19"/>
+            <Cards data={data}/>
+            <CountryPicker handleCountryChange={this.handleCountryChange}/>
+            <Chart data={data} country={country}/>
+            </div>
+            
+        )
+    }
+}
+export default App;
